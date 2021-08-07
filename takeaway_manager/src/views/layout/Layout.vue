273f42261301138cbb9d5dc6a190fd04269b38c7
EvenfallDew <evenfalldew@outlook.com>
2021-08-08 02:07:24 +0800

    v1.02 add BgColor

--- takeaway_manager/src/views/layout/Layout.vue
+++ takeaway_manager/src/views/layout/Layout.vue
@@ -1,9 +1,12 @@
 <template>
-	<div class="container">
+	<div class="container" ref="bg">
 		<!-- 左边部分 -->
 		<aside>
-			<h2 class="title">外卖商家中心</h2>
-			<LeftMenu></LeftMenu>
+			<div>
+				<h2 class="title">外卖商家中心</h2>
+				<LeftMenu></LeftMenu>
+			</div>
+			<div class="change-color" @click="change()">狗狗祟祟，换个主题</div>
 		</aside>
 		<!-- 右边部分 -->
 		<div class="content">
@@ -25,6 +28,29 @@ export default {
 	components: {
 		LeftMenu,
 		TopNav,
+	},
+
+	data() {
+		return {
+			bgColor: [
+				"linear-gradient(45deg, #0c675e, #069e90)",
+				"linear-gradient(45deg, #29323c, #485563)",
+				"linear-gradient(45deg, #795548, #945c48)",
+				"linear-gradient(45deg, #1565C0, #1E88E5)",
+				"linear-gradient(45deg, #65379b, #886aea)",
+			],
+			colorNum: 0,
+		};
+	},
+
+	methods: {
+		change() {
+			if (this.colorNum == this.bgColor.length) {
+				this.colorNum = 0;
+			}
+			this.$refs.bg.style.backgroundImage = this.bgColor[this.colorNum];
+			this.colorNum = this.colorNum + 1;
+		},
 	},
 };
 </script>
