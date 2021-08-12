2a21e0d6c8c1f59c4250c9d42c7b4b84af85f478
EvenfallDew <40364969+EvenfallDew@users.noreply.github.com>
2021-08-12 21:25:14 +0800

    create

---
+++ fitness_vant/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id="app">
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+import 
+export default {
+
+};
+</script>
+
+<style lang="less">
+#app {
+}
+</style>
---
+++ fitness_vant/src/components/Tobbar.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="tobbar">
+		<van-tabbar route v-model="active" active-color="#29a1f7" inactive-color="#000">
+			<van-tabbar-item replace to="/home" icon="home-o">标签</van-tabbar-item>
+			<van-tabbar-item replace to="/search" icon="search">标签</van-tabbar-item>
+			<van-tabbar-item replace to="/friends" icon="friends-o">标签</van-tabbar-item>
+			<van-tabbar-item replace to="/setting" icon="contact">标签</van-tabbar-item>
+		</van-tabbar>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.tobbar {
+	background-color: aquamarine;
+}
+</style>
