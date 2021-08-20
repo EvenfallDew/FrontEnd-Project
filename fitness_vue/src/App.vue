91873078f275d86db30ec324022b0a735f26ef43
EvenfallDew <40364969+EvenfallDew@users.noreply.github.com>
2021-08-20 18:02:48 +0800

    11

--- fitness_vue/src/App.vue
+++ fitness_vue/src/App.vue
@@ -1,17 +1,38 @@
 <template>
 	<div id="app">
+		<van-skeleton title avatar :row="3" :loading="loading">
+			<div class="haha">实际内容</div>
+		</van-skeleton>
+
+		<van-button plain type="primary" @click="change()">朴素按钮</van-button>
 		<router-view></router-view>
 	</div>
 </template>
 
 <script>
-import 
 export default {
-
+	data() {
+		return {
+			loading: true,
+		};
+	},
+	methods: {
+		change() {
+			this.loading = !this.loading;
+		},
+	},
 };
 </script>
 
 <style lang="less">
 #app {
+	.haha {
+		background-color: red;
+	}
+	@media screen and (max-width: 768px) {
+		.haha {
+			background-color: green;
+		}
+	}
 }
 </style>
